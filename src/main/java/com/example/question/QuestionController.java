06ee5f110e50cbd2f5e87b49f9a59732317c2fd0
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-10 17:06:58 +0900

    [Feat] chapter 2-10 질문 상세 페이지 생성

--- src/main/java/com/example/question/QuestionController.java
+++ src/main/java/com/example/question/QuestionController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,6 +21,14 @@ public class QuestionController {
 		List<Question> questionList = questionService.getList();
 		model.addAttribute(questionList);
 		return "question_list";
+	}
+	
+	// URL을 통해, id 값을 받아와 게시글 하나의 정보를 가져와 보여주는 메서드
+	@GetMapping("/question/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		Question question = questionService.getQuestion(id);
+		model.addAttribute(question);
+		return "question_detail";
 	}
 	
 	
