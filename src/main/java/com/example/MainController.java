273464aed4c9eaf63a6c082bfa4245c55ed7da65
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-10 16:52:03 +0900

    [Feat] chapter 2-08 루트 URL 질문 게시판으로 리다이렉트

--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -11,4 +11,10 @@ public class MainController {
 	public String index() {
 		return "안녕하세요 ssb에 오신 것을 환영합니다.";
 	}
+	
+	// 루트 URL 호출시 질문 리스트로 리다이렉트 시켜주는 메서드 
+	@GetMapping("/")
+	public String root() {
+		return "redirect:/question/list";
+	}
 }
--- src/main/java/com/example/question/QuestionController.java
+++ src/main/java/com/example/question/QuestionController.java
@@ -15,10 +15,13 @@ public class QuestionController {
 	
 	private final QuestionRepository questionRepository;
 	
+	// Question 테이블의 모든 데이터를 조회해 thymeleaf에 셋팅 하는 메서드
 	@GetMapping("/question/list")
 	public String list(Model model) {
 		List<Question> questionList = this.questionRepository.findAll();
 		model.addAttribute(questionList);
 		return "question_list";
 	}
+	
+	
 }
