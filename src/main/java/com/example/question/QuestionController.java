a1f632965f1482a9139c499f66c3b63f402e5621
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-10 17:10:13 +0900

    [Feat] chapter 2-11 URL 프리픽스 설정하기

--- src/main/java/com/example/question/QuestionController.java
+++ src/main/java/com/example/question/QuestionController.java
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
+// prefix URL 설정
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
@@ -16,7 +19,7 @@ public class QuestionController {
 	private final QuestionService questionService;
 	
 	// Question 테이블의 모든 데이터를 조회해 thymeleaf에 셋팅 하는 메서드
-	@GetMapping("/question/list")
+	@GetMapping("/list")
 	public String list(Model model) {
 		List<Question> questionList = questionService.getList();
 		model.addAttribute(questionList);
@@ -24,7 +27,7 @@ public class QuestionController {
 	}
 	
 	// URL을 통해, id 값을 받아와 게시글 하나의 정보를 가져와 보여주는 메서드
-	@GetMapping("/question/detail/{id}")
+	@GetMapping("/detail/{id}")
 	public String detail(Model model, @PathVariable("id") Integer id) {
 		Question question = questionService.getQuestion(id);
 		model.addAttribute(question);
